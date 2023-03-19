e1ec35989d116c8e05584931b797cca81be74a73
Evn <123427238+Evn08@users.noreply.github.com>
2023-03-19 02:52:59 +0000

    tweak: Name FINAL!!

--- winutil.ps1
+++ winutil.ps1
@@ -835,9 +835,11 @@ Function Invoke-WPFFormVariables {
 
 
     Write-Host ""
-    Write-Host " ░█▀▀▀ ░█──░█ ░█▄─░█ "
-    Write-Host " ░█▀▀▀ ─░█░█─ ░█░█░█ "
-    Write-Host " ░█▄▄▄ ──▀▄▀─ ░█──▀█"
+    Write-Host "¶¶¶¶¶````¶¶``¶¶````¶¶``¶¶"     
+    Write-Host "¶¶```````¶¶``¶¶````¶¶¶`¶¶"
+    Write-Host "¶¶¶¶`````¶¶``¶¶````¶¶`¶¶¶"
+    Write-Host "¶¶````````¶¶¶¶`````¶¶``¶¶"
+    Write-Host "¶¶¶¶¶``````¶¶``````¶¶``¶¶"
     Write-Host ""
     Write-Host "====Evn08====="
     Write-Host "=====Windows Toolbox====="
